12b75ad7f35fbcdae6535cf850a01fc54f5fbadf
furmi <147946906+Furmi37@users.noreply.github.com>
2024-03-16 17:15:13 +0100

    #Commit

---
+++ src/main/java/com/furmi/LeetcodeSumofTwoIntegers.java
@@ -0,0 +1,19 @@
+package com.furmi;
+
+public class LeetcodeSumofTwoIntegers {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+        return res;
+    }
+    public boolean isPalindrome(int x) {
+        Integer.
+    }
+}
