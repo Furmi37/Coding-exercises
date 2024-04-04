90da0969eca345fac6dfb8db35c602945b2631bd
furmi <147946906+Furmi37@users.noreply.github.com>
2024-04-04 12:39:57 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/ReverseString.java
@@ -0,0 +1,15 @@
+package com.furmi;
+
+public class ReverseString {
+    public static void main(String[] args) {
+    }
+//hello
+    public void reverseString(char[] s) {
+
+        for (int i = 0; i < s.length / 2; i++){
+            char temp = s[i];
+            s[i] = s[s.length-1-i];
+            s[s.length-1-i] = temp;
+        }
+    }
+}
