d2a793ad8062c5ac48cac3e25cd4cf60c3d2b536
furmi <147946906+Furmi37@users.noreply.github.com>
2024-04-04 12:43:56 +0200

    #Practicing exercises

--- src/main/java/com/furmi/ReverseInteger.java
+++ src/main/java/com/furmi/ReverseInteger.java
@@ -2,21 +2,37 @@ package com.furmi;
 
 public class ReverseInteger {
     public static void main(String[] args) {
-        System.out.println(reverse(-123));
+        System.out.println(reverseInt(-123));
 
     }
 
-    public static int reverse(int x) {
+    public static int reverseInt(int x) {
         int result = 0;
 
         while (x != 0) {
-            int tail = x % 10;
-            int newResult = result * 10 + tail;
-            if ((newResult - tail) / 10 != result)  // check if overflows!!!
+            int tail = x % 10; // bieremy ostatnio cyfre
+            int newResult = result * 10 + tail; // dajemy jako 1 w nowym wyniku
+            if ((newResult - tail) / 10 != result)  // check if overflows!!! sprawdzamy czy
             { return 0; }
             result = newResult;
             x = x / 10;
         }
         return result;
+    }
+    public int reverse(int x) {
+        boolean isNegative = false;
+        if (x < 0) {
+            isNegative = true;
+            x = -x;
+        }
+        long reverse = 0;
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+        if (reverse > Integer.MAX_VALUE) {
+            return 0;
+        }
+        return (int) (isNegative ? -reverse : reverse);
     }
 }
