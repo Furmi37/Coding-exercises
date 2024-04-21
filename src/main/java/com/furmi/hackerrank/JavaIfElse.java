923e4940a980a168c44f3349edd5efd1048630f6
furmi <147946906+Furmi37@users.noreply.github.com>
2024-04-21 10:02:48 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/hackerrank/JavaIfElse.java
@@ -0,0 +1,20 @@
+package com.furmi.hackerrank;
+
+public class JavaIfElse {
+    public static void main(String[] args) {
+        System.out.println(ifElse(3));
+
+    }
+
+    public static String ifElse(int n){
+        if (n % 2 != 0){
+            return "Weird";
+        }else if (n % 2 ==0 && (n > 1 && n <6)){
+            return "Not Werid";
+        }else if (n % 2 == 0 && (n > 5 && n < 21)){
+            return "Weird";
+        }else{
+            return "Not Weird";
+        }
+    }
+}
