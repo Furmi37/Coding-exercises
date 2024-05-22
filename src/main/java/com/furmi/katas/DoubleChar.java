0849b433ffccdbe01e84f402a0e376f32fadea1c
furmi <147946906+Furmi37@users.noreply.github.com>
2024-05-22 17:58:11 +0200

    #Practicing exercises

--- src/main/java/com/furmi/katas/DoubleChar.java
+++ src/main/java/com/furmi/katas/DoubleChar.java
@@ -7,6 +7,7 @@ public class DoubleChar {
         for (int i =0; i < s.length();i++){
             result += s.charAt(i);
             for (int j =0; j <1; j++){
+
                 result += s.charAt(i);
             }
         }
