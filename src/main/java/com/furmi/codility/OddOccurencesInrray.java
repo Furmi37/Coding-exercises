ade5dd50a8cd8049e6bdfa90d8501a14375e6bf6
furmi <147946906+Furmi37@users.noreply.github.com>
2024-06-18 15:49:13 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/codility/OddOccurencesInrray.java
@@ -0,0 +1,21 @@
+package com.furmi.codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class OddOccurencesInrray {
+
+    public int solution(int[] A) {
+        // Implement your solution here
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < A.length; i++){
+            if (set.contains(A[i])){
+                set.remove(A[i]);
+            }else {
+                set.add(A[i]);
+            }
+        }
+        Object[] array = set.toArray();
+        return (int)array[0];
+    }
+}
---
+++ src/main/java/com/furmi/hackerrank/RollerSkates.java
@@ -0,0 +1,12 @@
+package com.furmi.hackerrank;
+
+public class RollerSkates {
+    static int wheels = 1;
+    int tracks =5;
+
+    public static void main(String[] args) {
+        RollerSkates rollerSkates = new RollerSkates();
+        int feet = 4, tracks =15;
+        System.out.println(feet + tracks + rollerSkates.wheels);
+    }
+}
