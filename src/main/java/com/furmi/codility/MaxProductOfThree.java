0bfd526db0650c5bffbdeef04fb75713f07693a4
furmi <147946906+Furmi37@users.noreply.github.com>
2024-06-19 11:12:25 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/codility/MaxProductOfThree.java
@@ -0,0 +1,13 @@
+package com.furmi.codility;
+
+import java.util.Arrays;
+
+public class MaxProductOfThree {
+    public int solution(int[] A) {
+        // Implement your solution here
+        int max = 0;
+        Arrays.sort(A);
+
+        return Math.max(A[0]*A[1]*A[A.length-1],A[A.length-3]*A[A.length-2]* A[A.length-1]);
+    }
+}
