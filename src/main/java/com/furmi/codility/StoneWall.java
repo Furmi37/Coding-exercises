a1dd38a95d9f595a889422e607f3202b6fd3956b
furmi <147946906+Furmi37@users.noreply.github.com>
2024-06-19 13:53:47 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/codility/StoneWall.java
@@ -0,0 +1,29 @@
+package com.furmi.codility;
+
+import java.util.Stack;
+
+public class StoneWall {
+    public int solution(int[] H) {
+        // Implement your solution here
+        Stack <Integer> stack = new Stack<>();
+        int numBlocks = 0;
+
+        for (int i = 0; i < H.length; i++){
+
+            while(stack.isEmpty() == false && stack.peek() > H[i]){
+                stack.pop();
+            }
+
+            if (stack.isEmpty()){
+                numBlocks++;
+                stack.push(H[i]);
+            }else if (stack.peek() == H[i]){
+            }
+            else if (stack.peek() < H[i]){
+                numBlocks++;
+                stack.push(H[i]);
+            }
+        }
+        return numBlocks;
+    }
+}
