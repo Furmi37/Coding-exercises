372a632352da3f50954a2171b39dda5e32d48dad
furmi <147946906+Furmi37@users.noreply.github.com>
2024-06-19 12:18:07 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/codility/Fish.java
@@ -0,0 +1,32 @@
+package com.furmi.codility;
+
+import java.util.Stack;
+
+public class Fish {
+    public int solution(int[] A, int[] B) {
+        // Implement your solution here
+        Stack<Integer> stack = new Stack<>();
+
+        int numAlive = A.length;
+
+        for (int i = 0; i < A.length; i++){
+            if (B[i] == 1){
+                stack.push(A[i]);
+            }
+            if (B[i] == 0){
+                while (!stack.isEmpty()){
+                    if (stack.peek() > A[i]){
+                        numAlive--;
+                        break;
+                    }
+                    else if (stack.peek() < A[i]){
+                        numAlive--;
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return numAlive;
+    }
+
+}
