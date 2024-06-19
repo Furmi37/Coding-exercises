4d6117aeede7484dbe559e4cbfdcabf845e1bc70
furmi <147946906+Furmi37@users.noreply.github.com>
2024-06-19 10:50:16 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/codility/Distinct.java
@@ -0,0 +1,18 @@
+package com.furmi.codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Distinct {
+    public int solution(int[] A) {
+        // Implement your solution here
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : A){
+            if (!set.contains(i)){
+                set.add(i);
+            }
+        }
+        return set.size();
+    }
+}
