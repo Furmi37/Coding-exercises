cdb8a5313e6bd54589c8baacaf0c333673e8883e
furmi <147946906+Furmi37@users.noreply.github.com>
2024-06-24 17:09:45 +0200

    #Practicing exercises

--- src/main/java/com/furmi/codilityKurs/CountFactors.java
+++ src/main/java/com/furmi/codilityKurs/CountFactors.java
@@ -1,18 +1,40 @@
 package com.furmi.codilityKurs;
 
 public class CountFactors {
-    public int solution(int N) {
+    public static void main(String[] args) {
+        System.out.println(solution(25));
+        System.out.println(sol2(25));
+    }
+
+    public static int solution(int N) {
         // Implement your solution here
         int factor = 0;
-        if (N ==0){
-            return 0;
+        if (N == 1) {
+            return 1;
         }
-
-        for (int i = 1; i <= N; i++){
-            if (N % i == 0){
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
                 factor++;
             }
         }
+        return factor;
+    }
+
+    public static int sol2(int N) {
+        int sqrtN = (int) Math.sqrt(N);
+        int factor = 0;
+
+        for (int i = 1; i <= sqrtN; i++) {
+            if (N % i == 0) {
+                factor++;
+            }
+        }
+        factor *= 2;
+
+        if (sqrtN * sqrtN == N) {
+            factor = factor - 1;
+        }
+
         return factor;
     }
 }
