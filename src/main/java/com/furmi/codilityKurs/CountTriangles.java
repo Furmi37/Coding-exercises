55ebe0ce25e6c829401959163e233caa098d4471
furmi <147946906+Furmi37@users.noreply.github.com>
2024-06-25 11:45:18 +0200

    #Practicing exercises

--- src/main/java/com/furmi/codilityKurs/CountTriangles.java
+++ src/main/java/com/furmi/codilityKurs/CountTriangles.java
@@ -1,5 +1,7 @@
 package com.furmi.codilityKurs;
 
+import java.util.Arrays;
+
 public class CountTriangles {
     public static void main(String[] args) {
         int [] arr = new int[]{10,2,5,1,8,12};
@@ -7,18 +9,21 @@ public class CountTriangles {
     }
     public static int solution(int[] A) {
         // Implement your solution here
-        if (A.length ==0){
-            return 0;
-        }
-        int triangles = 0;
+       int numTriangle =0;
+        Arrays.sort(A);
 
-        for (int i = 0; i < A.length; i++){
-            for (int j = i+1; j<A.length-1;){
-                if (A[i] < A[j] + A[j+1]){
-                    triangles++;
+        for (int i = 0; i < A.length-2; i++){
+            int leftEnd = i+1;
+            int rightEnd = i +2;
+            while(leftEnd < A.length-1){
+                if (rightEnd < A.length && A[i] + A[leftEnd] > A[rightEnd]){
+                    rightEnd++;
+                }else {
+                    numTriangle = numTriangle + (rightEnd-leftEnd-1);
+                    leftEnd++;
                 }
             }
         }
-        return triangles;
+        return numTriangle;
     }
 }
