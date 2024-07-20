4d7d66b78e24d2a505515a8235246012f60a044b
furmi <147946906+Furmi37@users.noreply.github.com>
2024-07-20 18:16:55 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/katas/Grasshopper_MessiGoals.java
@@ -0,0 +1,13 @@
+package com.furmi.katas;
+
+public class Grasshopper_MessiGoals {
+    class Goals {
+        public static int laLigaGoals = 43;
+        public static int championsLeagueGoals = 10;
+        public static int copaDelReyGoals = 5;
+
+        public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+    }
+
+}
