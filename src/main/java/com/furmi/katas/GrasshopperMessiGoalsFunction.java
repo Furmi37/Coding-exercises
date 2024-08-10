271d2599223c9528f3cb47cf86d89292a1820fbb
furmi <147946906+Furmi37@users.noreply.github.com>
2024-08-10 10:36:17 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/katas/GrasshopperMessiGoalsFunction.java
@@ -0,0 +1,8 @@
+package com.furmi.katas;
+
+public class GrasshopperMessiGoalsFunction {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        // your code here
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
