56741eced3d805d2dfd30b685350056931662d78
furmi <147946906+Furmi37@users.noreply.github.com>
2024-08-15 12:24:27 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/katas/SentenceSmash.java
@@ -0,0 +1,19 @@
+package com.furmi.katas;
+
+public class SentenceSmash {
+    public static void main(String[] args) {
+        String [] s = new String[]{"hello", "world"};
+        smash(s);
+    }
+    public static String smash(String... words) {
+        // TODO Write your code below this comment please
+        if (words.length <= 0){
+            return "";
+        }
+        String result = words[0];
+        for (int i = 1; i < words.length; i++){
+            result += " " + words[i];
+        }
+        return result;
+    }
+}
