14cf779b8e20a1fafbdc8b47058849e5e25f5533
furmi <147946906+Furmi37@users.noreply.github.com>
2024-09-03 19:03:36 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/katas/GrasshoperCheckForFactor.java
@@ -0,0 +1,10 @@
+package com.furmi.katas;
+
+public class GrasshoperCheckForFactor {
+
+    public static boolean checkForFactor(int base, int factor) {
+        // your code here
+
+        return base % factor == 0? true : false;
+    }
+}
