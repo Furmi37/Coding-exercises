801dd500d2d88be72af2eb6fd07aea740f5ef5ba
furmi <147946906+Furmi37@users.noreply.github.com>
2024-09-06 21:42:09 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/katas/GrasshoperGameMoveFunction.java
@@ -0,0 +1,9 @@
+package com.furmi.katas;
+
+public class GrasshoperGameMoveFunction {
+
+    public static int move(int position, int roll) {
+        // your code here
+        return position + (roll*2);
+    }
+}
