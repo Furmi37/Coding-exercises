97d5c9fa2fcf7a4e2f5756d5a2ada362ccb189f1
furmi <147946906+Furmi37@users.noreply.github.com>
2024-09-19 15:08:46 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/katas/VolumeOfACubiod.java
@@ -0,0 +1,9 @@
+package com.furmi.katas;
+
+public class VolumeOfACubiod {
+
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        // Your code here
+        return (length * width) * height;
+    }
+}
