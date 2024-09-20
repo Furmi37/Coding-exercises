ca96f0314906436e9ba8623b650656cfe97aca71
furmi <147946906+Furmi37@users.noreply.github.com>
2024-09-20 11:06:35 +0200

    #Practicing exercises

---
+++ src/main/java/com/furmi/katas/YouCantCodeUnderPressure1.java
@@ -0,0 +1,8 @@
+package com.furmi.katas;
+
+public class YouCantCodeUnderPressure1 {
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i*2;
+    }
+}
---
+++ src/main/java/com/furmi/katas/YouCantCodeUnderPressure2.java
@@ -0,0 +1,13 @@
+package com.furmi.katas;
+
+public class YouCantCodeUnderPressure2 {
+    private int num = 0;
+    public int check(){
+        return num;
+    }
+
+    public void increment (){
+        num++;
+    }
+
+}
