74508980d0c6c47bbc8b688ed0bbaa4ece6a9f7f
furmi <147946906+Furmi37@users.noreply.github.com>
2024-09-26 14:48:32 +0200

    #Practicing exercises

--- src/main/java/com/furmi/codilityKurs/CyclicRotation.java
+++ src/main/java/com/furmi/codilityKurs/CyclicRotation.java
@@ -10,21 +10,35 @@ public class CyclicRotation {
 
     public static int[] solution(int[] A, int K) {
         // Implement your solution here
-        int []res = new int[A.length];
-        int length = A.length;
-
-
-        if (length == 0){
-            return res;
+//        int []res = new int[A.length];
+//        int length = A.length;
+//
+//
+//        if (length == 0){
+//            return res;
+//        }
+//        int index = 0;
+//
+//        for(int i = length - K; i < length; i++){
+//            res[index++]= A[i];
+//        }
+//        for (int i = 0; i < length-K; i++) {
+//            res[index++] = A[i];
+//        }
+//        return res;
+        if (A.length == 0){
+            return A;
         }
-        int index = 0;
+        int temp, prev;
 
-        for(int i = length - K; i < length; i++){
-            res[index++]= A[i];
+        for (int i = 0; i < K; i++){
+            temp = A[A.length-1];
+            for (int j =0; j < A.length; j++){
+                prev = A[j];
+                A[j] = temp;
+                temp = prev;
+            }
         }
-        for (int i = 0; i < length-K; i++) {
-            res[index++] = A[i];
-        }
-        return res;
+        return A;
     }
 }
